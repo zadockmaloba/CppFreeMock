d5610c0a5f63638c412637da5a6c3626144a3251
zadockmaloba <malobazadock@gmail.com>
2023-09-05 17:22:00 +0300

    WIP

--- arm/runtime_patch_impl.h
+++ arm/runtime_patch_impl.h
@@ -62,7 +62,7 @@ namespace RuntimePatcherImpl {
         using namespace hooker;
         std::unique_ptr<HookerFactory> factory = HookerFactory::getInstance();
         const Hooker& hooker = factory->getHooker();
-        hooker.hook(function, replacement, NULL);
+        hooker.hook(function, replacement, &function);
     }
 
     static int SetJump(void* address, void* destination, std::vector<char>& binary_backup) {
