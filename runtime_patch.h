d8d8785a96ae13cc1f952afbafa0c280bf0ba67f
Zadock Maloba <malobazadock@gmail.com>
2023-09-06 12:07:15 +0300

    Revert some changes in runtime_patch.h

--- runtime_patch.h
+++ runtime_patch.h
@@ -44,9 +44,14 @@ namespace CppFreeMock {
 struct RuntimePatcher {
     template < typename F1, typename F2 >
     static int GraftFunction(F1 address, F2 destination, std::vector<char>& binary_backup) {
-        void* original_function = reinterpret_cast<void*>(address);
-        void* mock_function     = reinterpret_cast<void*>(destination);
-        RuntimePatcherImpl::SetJump(original_function, mock_function, binary_backup);
+        void* function = reinterpret_cast<void*>((std::size_t&)address);
+        if (0 != RuntimePatcherImpl::UnprotectMemoryForOnePage(function)) {
+            //std::abort();
+            throw "Unable to unprotect page\n";
+        } else {
+            // For mock std::abort, this need not after the 'if'.
+            return RuntimePatcherImpl::SetJump(function, reinterpret_cast<void*>((std::size_t&)destination), binary_backup);
+        }
     }
 
     template < typename F >
